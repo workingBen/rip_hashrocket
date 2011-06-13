e0be0538dd0b07214f310dbfd80444593080395f
Ben Pellow <ben@campuscred.com>
2011-06-13 02:07:48 -0700

    add to test1

--- test_rb_files/test1.rb
+++ test_rb_files/test1.rb
@@ -1,3 +1,11 @@
 {:a => 'a', :b => 'b', :c => 'c'}
 {:d => 'd', :e => 'e', :f => 'f'}
 
+
+         :name         => "Someone",
+         :address1     => "10 Somewhere Place",
+         :city         => "ACity",
+         :state        => "CA",
+         :country      => "US",
+         :zip          => "91634",
+
