abc3d6a4fa91f947ce551b2e97bc93c8c34a6eba
Ben Pellow <ben@campuscred.com>
2011-06-12 21:54:50 -0700

    add backup option and properly delete and rewrite files

--- lib/rip_hashrocket.rb
+++ lib/rip_hashrocket.rb
@@ -4,22 +4,36 @@ module RipHashrocket
   def self.process(options)
   
     directory = options[0] || Dir.pwd
+    backup = options[1] || false
 
     # TODO : add options for recursivity (and application of the script to a single file)
     rbfiles = File.join(directory ,"**", "*.rb")
     Dir.glob(rbfiles).each do |filename|
       file = File.new(filename, "r+")
       
+      made_changes = false
       lines = file.readlines
 
       lines.each_with_index do |line, i|
         newline = line.replace_rockets(line)
+        made_changes = true if lines[i] != newline
         lines[i] = newline
       end
-            
-      file.pos = 0
-      file.puts(lines.join)
+  
       file.close
+      if made_changes
+	      if backup
+          File.rename(filename, filename + ".bak")
+        else
+          File.delete(filename)
+        end
+
+	      file = File.new(filename, "w+")
+	
+	      file.rewind
+	      file.puts(lines.join)
+	      file.close
+      end
     end
     p "Hash Rockets replaced for #{Dir.glob(rbfiles).count} source files"    
   end
